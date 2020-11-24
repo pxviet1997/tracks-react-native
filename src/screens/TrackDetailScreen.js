5867f72b7a44039c254c60e839b05ac74a7d0c82
Viet Pham <vietpham@Viets-MacBook-Pro.local>
2020-11-24 22:33:30 +1100

    finsished with the TrackList and TrackDetail screens

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Context as TrackContext } from '../context/TrackContext';
+import MapView, { Polyline } from 'react-native-maps';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const _id = navigation.getParam('_id');
+
+  const track = state.find(track => track._id === _id);
+  const initialCoords = track.locations[0].coords;
+
   return (
-    <Text>Track Detail</Text>
+    <>
+      <Text style={{ fontSize: 48 }}>{track.name}</Text>
+      <MapView
+        initialRegion={{
+          ...initialCoords,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}
+        style={styles.map}
+      >
+        <Polyline coordinates={track.locations.map(location => location.coords)} />
+      </MapView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-
+  map: {
+    height: 300
+  }
 });
 
 export default TrackDetailScreen;
